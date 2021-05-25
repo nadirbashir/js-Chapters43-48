a04a15f7a87d8bb24f10ab0cde01612902a8048f
nadirbashir <1.nadirbashir@gmail.com>
2021-05-26 01:33:06 +0500

    task 4 complete

--- app.js
+++ app.js
@@ -1,39 +1,74 @@
-var names = [
-  "Ali",
-  "Ahmed",
-  "majid",
-  "Rehan",
-  "Arif",
-  "Zain",
-  "Hussain",
-  "Amir",
-  "ali",
-  "Majid",
-];
-var classes = [10, 9, 9, 10, 7, 8, 9, 7, 10, 8];
-document.write(
-  "<table id = 'myTable'> <tr> <th>Index</th> <th>Name</th> <th>Class</th> <th></th></tr>"
-);
-for (var i = 0; i < names.length; i++) {
-  document.write(
-    "<tr id = '" +
-      i +
-      "'><td>" +
-      i +
-      "</td> <td>" +
-      names[i] +
-      "</td> <td>" +
-      classes[i] +
-      "</td> <td><button onClick = 'deleteRow()'>Delete</button></td></tr>"
-  );
-}
+// var names = [
+//   "Ali",
+//   "Ahmed",
+//   "majid",
+//   "Rehan",
+//   "Arif",
+//   "Zain",
+//   "Hussain",
+//   "Amir",
+//   "ali",
+//   "Majid",
+// ];
+// var classes = [10, 9, 9, 10, 7, 8, 9, 7, 10, 8];
+// document.write(
+//   "<table id = 'myTable'> <tr> <th>Index</th> <th>Name</th> <th>Class</th> <th></th></tr>"
+// );
+// for (var i = 0; i < names.length; i++) {
+//   document.write(
+//     "<tr id = '" +
+//       i +
+//       "'><td>" +
+//       i +
+//       "</td> <td>" +
+//       names[i] +
+//       "</td> <td>" +
+//       classes[i] +
+//       "</td> <td><button onClick = 'deleteRow()'>Delete</button></td></tr>"
+//   );
+// }
 
-function deleteRow() {
-    var indeces = []
-    var rows = document.getElementById('myTable').getElementsByTagName('tbody')[0].getElementsByTagName('tr');
-    for (i = 0; i < rows.length - 1; i++) {
-        indeces.push(i)
+// function deleteRow() {
+//     var indeces = []
+//     var rows = document.getElementById('myTable').getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+//     for (i = 0; i < rows.length - 1; i++) {
+//         indeces.push(i)
+//     }
+//     console.log(indeces);
+//     document.getElementById("myTable").deleteRow();
+//   }
+
+// /*******task 4  ***********/
+
+function swap(id){
+    if(id === "s20"){
+    var image = document.getElementById(id);
+    image.src = "https://www.gizmochina.com/wp-content/uploads/2020/02/Samsung-Galaxy-S20-Ultra-500x500.jpg";
+    }else if(id === "12pro"){
+        document.getElementById(id).src="https://img.router-switch.com/media/customoptions/129/2/4/iphone-12-pro-max-blue.jpg";
+    }else if(id==="note20"){
+        document.getElementById(id).src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRodTTvSgOnB_sBLbrxtNOLCJSWJcB_-TyW3Z43gW0thDUpDOHp7oapHls26YXq6D2xoyI&usqp=CAU";
+    }else if(id==="nova7i"){
+        document.getElementById(id).src = "https://www.pakmobizone.pk/wp-content/uploads/2020/07/Huawei-nova-7i-skyline-grey.png";
     }
-    console.log(indeces);
-    document.getElementById("myTable").deleteRow();
-  }
+    else{
+        var image = document.getElementById("car");
+        image.src = "https://img.mensxp.com/media/photogallery/2020/Aug/most-expensive-cars-in-the-world-1400x653_5f33cd506d2a7.jpeg?w=820&h=540&cc=1"
+    }
+}
+function set(id){
+    if(id === "s20"){
+        document.getElementById(id).src = "https://cdn.homeshopping.pk/product_images/x/761/7101w7I2tIL._SX679___21187_zoom.jpg";
+        }else if(id === "12pro"){
+            document.getElementById(id).src = "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-12-pro-max-gold-hero?wid=940&hei=1112&fmt=png-alpha&.v=1604021660000";
+        }else if(id==="note20"){
+            document.getElementById(id).src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyas7l38bhNegFMyg4GE-u0sFdz1_XpEAUBQ&usqp=CAU";
+        }else if(id==="nova7i"){
+            document.getElementById(id).src = "https://lahorecentre.com/wp-content/uploads/2020/07/black-i.jpg";
+        }
+        else{
+            var image = document.getElementById("car");
+            image.src = "https://d3nv2arudvw7ln.cloudfront.net/images/952/372/slide1_MainMenu%20HP%20CAR.jpg"
+        }
+    
+}
