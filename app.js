7d78bafd98722a6b154eec5f9970b3ab8cd36b87
nadirbashir <1.nadirbashir@gmail.com>
2021-05-26 05:19:04 +0500

    update app.js

--- app.js
+++ app.js
@@ -55,44 +55,47 @@ function makeFieldwhite(counterFieldId){
 
 }
 
-// var names = [
-//   "Ali",
-//   "Ahmed",
-//   "majid",
-//   "Rehan",
-//   "Arif",
-//   "Zain",
-//   "Hussain",
-//   "Amir",
-//   "ali",
-//   "Majid",
-// ];
-// var classes = [10, 9, 9, 10, 7, 8, 9, 7, 10, 8];
-// document.write(
-//   "<table id = 'myTable'> <tr> <th>Index</th> <th>Name</th> <th>Class</th> <th></th></tr>"
-// );
-// for (var i = 0; i < names.length; i++) {
-//   document.write(
-//     "<tr id = '" +
-//       i +
-//       "'><td>" +
-//       i +
-//       "</td> <td>" +
-//       names[i] +
-//       "</td> <td>" +
-//       classes[i] +
-//       "</td> <td><button onClick = 'deleteRow()'>Delete</button></td></tr>"
-//   );
-// }
+var names = [
+  "Ali",
+  "Ahmed",
+  "majid",
+  "Rehan",
+  "Arif",
+  "Zain",
+  "Hussain",
+  "Amir",
+  "ali",
+  "Majid",
+];
+var classes = [10, 9, 9, 10, 7, 8, 9, 7, 10, 8];
+document.write(
+  "<table id = 'myTable'> <tr> <th>Index</th> <th>Name</th> <th>Class</th> <th></th></tr>"
+);
+for (var i = 0; i < names.length; i++) {
+  document.write(
+    "<tr id = '" +
+      i +
+      "'><td>" +
+      i +
+      "</td> <td>" +
+      names[i] +
+      "</td> <td>" +
+      classes[i] +
+      "</td> <td><button onClick = 'deleteRow("+i+")'>Delete</button></td></tr>"
+  );
+}
+document.write("</table>");
 
-// function deleteRow() {
-//     var indeces = []
-//     var rows = document.getElementById('myTable').getElementsByTagName('tbody')[0].getElementsByTagName('tr');
-//     for (i = 0; i < rows.length - 1; i++) {
-//         indeces.push(i)
-//     }
-//     console.log(indeces);
-//     document.getElementById("myTable").deleteRow();
-//   }
+function deleteRow(ind) {
+    var indeces = []
+    var rows = document.getElementById('myTable').getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+    document.getElementById("myTable").deleteRow(ind);
+    names.splice(ind,1)
+    classes.splice(ind,1)
+    for (var j = 0; j < rows.length ; j++) {
+        
+    }
+    console.log(indeces);
+  }
 
 // /*******task 4  ***********/
